04182edccfad982a7c5611739b9de12fdd481363
Magno <jmagno.dutra@hotmail.com>
2024-12-03 16:39:31 -0300

    188 - Managing related states

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,45 +1,31 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "dec") return state - action.payload;
-  if (action.type === "setCount") return action.payload;
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("Unknow Action");
+  }
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date();
   date.setDate(date.getDate() + count);
-
-  const dec = function () {
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
-    dispatch({ type: "dec", payload: 1 });
-  };
-
-  const inc = function () {
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
-    dispatch({ type: "inc", payload: 1 });
-  };
-
-  const defineCount = function (e) {
-    // setCount(Number(e.target.value));
-    dispatch({ type: "setCount", payload: Number(e.target.value) });
-  };
-
-  const defineStep = function (e) {
-    setStep(Number(e.target.value));
-  };
-
-  const reset = function () {
-    // setCount(0);
-    setStep(1);
-  };
 
   return (
     <div className="counter">
@@ -49,21 +35,28 @@ function DateCounter() {
           min="0"
           max="10"
           value={step}
-          onChange={defineStep}
+          onChange={(e) =>
+            dispatch({ type: "setStep", payload: Number(e.target.value) })
+          }
         />
         <span>{step}</span>
       </div>
 
       <div>
-        <button onClick={dec}>-</button>
-        <input value={count} onChange={defineCount} />
-        <button onClick={inc}>+</button>
+        <button onClick={() => dispatch({ type: "dec" })}>-</button>
+        <input
+          value={count}
+          onChange={(e) =>
+            dispatch({ type: "setCount", payload: Number(e.target.value) })
+          }
+        />
+        <button onClick={() => dispatch({ type: "inc" })}>+</button>
       </div>
 
       <p>{date.toDateString()}</p>
 
       <div>
-        <button onClick={reset}>Reset</button>
+        <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
       </div>
     </div>
   );
